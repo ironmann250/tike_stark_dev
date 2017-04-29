0d0123ab87745cfb1ef99947149ebeb4be8d6578
robo <berwa05@gmail.com>
2017-04-29 15:40:04 +0200

    Add files via upload

---
+++ tickapp/models.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+
+
+class Show(models.Model):
+    idshow = models.CharField(max_length=10,primary_key=True)
+    title = models.CharField(max_length=100)
+    Description = models.TextField()
+    date = models.DateTimeField()
+    venue = models.CharField(max_length=100, default="0")
+    tickets_no = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Admin:
+    pass
+
+
+class profile(models.Model):
+    seller = models.ForeignKey(User)
+    iduser = models.CharField(primary_key=True, max_length = 40)
+    event = models.ForeignKey(Show, default="0")
+
+
+''' @receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+	'''
+
+
+class Admin:
+    pass
+
+
+class tickettype(models.Model):
+    tike_types = models.CharField(max_length=30)
+    event = models.ForeignKey(Show)
+    amount = models.IntegerField()
+    idticktype = models.CharField(primary_key=True,max_length =30)
+
+    def __str__(self):
+        return self.tike_type
+
+
+class Admin:
+    pass
+
+
+class ticket(models.Model):
+    idticket = models.CharField(max_length=20, primary_key= True)
+    event = models.ForeignKey(Show, null=True)
+    phone_number = models.IntegerField(null=True)
+    pin = models.CharField(max_length=10)
+    email = models.EmailField()
+    seller = models.ForeignKey(profile, default="0", null=True)
+    Name = models.CharField(max_length=100, default="0")
+    ticket_type = models.ForeignKey(tickettype, default="0", null=True)
+    status = models.BooleanField(default=False)
+    
+
+
+class Admin:
+    pass
