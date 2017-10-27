fd03556aa5fb4575b954f166311bef1c9a381eb0
ironmann350 <ironmann350@outlook.com>
2017-10-27 22:41:26 +0800

    Update settings.py

--- tikecom/settings.py
+++ tikecom/settings.py
@@ -127,19 +127,18 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/sell'
 LOGIN_URL = '/login/'
-
+'''
 import dj_database_url
 DATABASES ={}
 DATABASES['default'] = dj_database_url.config()
-
+'''
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
 DEBUG = True
-'''
+
 try:
     from .local_settings import *
 except ImportError:
     pass
-'''
